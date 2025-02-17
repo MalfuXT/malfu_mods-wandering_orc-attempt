fc3e7d4fe5e492b42b1f88286fa872462ef8775d
Malfu <@MalfuXT>
2025-02-17 19:40:08 +0700

    sound update

--- src/main/java/malfu/wandering_orc/sound/ModSounds.java
+++ src/main/java/malfu/wandering_orc/sound/ModSounds.java
@@ -12,6 +12,14 @@ public class ModSounds {
     public static SoundEvent ORC_HURT = registerSoundEvent("orc_hurt");
     public static SoundEvent ORC_HURT2 = registerSoundEvent("orc_hurt2");
     public static SoundEvent ORC_ARCHER_PUNCH = registerSoundEvent("orc_archer_punch");
+    public static SoundEvent MINO_GROWL = registerSoundEvent("mino_growl");
+    public static SoundEvent MINO_DEATH = registerSoundEvent("mino_death");
+    public static SoundEvent MINO_HIT = registerSoundEvent("mino_hit");
+    public static SoundEvent MINO_STOMP = registerSoundEvent("mino_stomp");
+    public static SoundEvent MINO_HURT = registerSoundEvent("mino_hurt");
+    public static SoundEvent TROLL_HURT = registerSoundEvent("troll_hurt");
+    public static SoundEvent TROLL_DEATH = registerSoundEvent("troll_death");
+    public static SoundEvent SWING_SOUND = registerSoundEvent("swing_sound");
 
 
     private static SoundEvent registerSoundEvent(String name) {
